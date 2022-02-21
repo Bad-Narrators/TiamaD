37e2fc87a45500113d623691d90bb60a99028aec
LuciusForKing <71907618+BadNarrators@users.noreply.github.com>
2022-02-21 14:42:20 +0100

    Add files via upload

---
+++ tiamad/src/main/java/com/badnarrators/tiamad/TiamadApplication.java
@@ -0,0 +1,13 @@
+package com.badnarrators.tiamad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiamadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiamadApplication.class, args);
+	}
+
+}
---
+++ tiamad/src/test/java/com/badnarrators/tiamad/TiamadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.badnarrators.tiamad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiamadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
